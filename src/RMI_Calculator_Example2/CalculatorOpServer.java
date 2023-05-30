da65b0fc96f7ef6401bb7d09a0bf1f19d70a9e87
Gokul Pathak <gokul_pathak@yahoo.com>
2023-05-30 21:14:57 +0545

    create server class that register CalcatorOpImplemnt obj and start RMI reg.

---
+++ src/RMI_Calculator_Example2/CalculatorOpServer.java
@@ -0,0 +1,22 @@
+package RMI_Calculator_Example2;
+
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class CalculatorOpServer {
+    public static void main(String[] args) {
+        try{
+            CalculatorOperation calculatorOperation = new CalculatorOpeImplement();
+            //Start the RMI registry at port 1099
+            Registry registry = LocateRegistry.createRegistry(1099);
+
+            //Bind the calculator object to the registry
+            registry.rebind("Calculator", calculatorOperation);
+            System.out.println("Calculator Server is running....");
+        }
+        catch (RemoteException e){
+            e.printStackTrace();
+        }
+    }
+}
