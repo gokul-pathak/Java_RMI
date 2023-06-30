7e2ff50a053b85561452e1aec92f12a43fe245f5
Gokul Pathak <gokul_pathak@yahoo.com>
2023-06-30 17:41:15 +0545

    Contains the main function to start the program

---
+++ src/MultiThreadedFileDownloader/Main.java
@@ -0,0 +1,8 @@
+package MultiThreadedFileDownloader;
+
+public class Main {
+    public static void main(String[] args) {
+        FileDownloaderApp fileDownloaderApp = new FileDownloaderApp();
+        fileDownloaderApp.start();
+    }
+}
