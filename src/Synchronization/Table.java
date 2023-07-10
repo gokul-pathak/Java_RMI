2fb68af032a50b531eda591982c85fb9312e4b3f
Gokul Pathak <gokul_pathak@yahoo.com>
2023-07-10 08:50:41 +0545

    synchronized method added

--- src/Synchronization/Table.java
+++ src/Synchronization/Table.java
@@ -1,15 +1,29 @@
 package Synchronization;
 
 public class Table {
-    synchronized void printTable(int n) //
-    {
-        for (int i = 1; i<=5; i++){
-            System.out.println(n*i);
-            try{
-                Thread.sleep(500);
+//    synchronized void printTable(int n) //
+//    {
+//        for (int i = 1; i<=5; i++){
+//            System.out.println(n*i);
+//            try{
+//                Thread.sleep(500);
+//
+//            }catch(InterruptedException ie){
+//                System.out.println(ie);
+//            }
+//        }
+//    }
 
-            }catch(InterruptedException ie){
-                System.out.println(ie);
+    void printTable(int n) {
+        synchronized (this) {
+            for (int i = 1; i <= 5; i++) {
+                System.out.println(n * i);
+                try {
+                    Thread.sleep(500);
+
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
             }
         }
     }
